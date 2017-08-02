27535d1b438e2be4c58bbc070c5203ad1dda4c0a
Lukas Bosse <LukasBosse95@gmail.com>
2017-08-03 01:37:20 +0200

    Init Entity

--- app/src/main/java/de/schulze/bosse/fallingstones/GameActivity.java
+++ app/src/main/java/de/schulze/bosse/fallingstones/GameActivity.java
@@ -1,8 +1,10 @@
 package de.schulze.bosse.fallingstones;
 
 import android.app.Activity;
+import android.graphics.Point;
 import android.opengl.GLSurfaceView;
 import android.os.Bundle;
+import android.view.Display;
 import com.twicecircled.spritebatcher.Drawer;
 import com.twicecircled.spritebatcher.SpriteBatcher;
 import de.schulze.bosse.entites.Background;
@@ -11,10 +13,16 @@ import javax.microedition.khronos.opengles.GL10;
 
 public class GameActivity extends Activity implements Drawer {
 
+	//Controller
+	private boolean running = true;
+	//Display
+	private Display display;
+	private Point displaySize;
 	//Resource-Ids
 	private int[] resourceIds = { R.drawable.gameBackground };
 	//Background
-	private Background background;
+	private Background firstBackground;
+	private Background secondBackground;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -22,16 +30,32 @@ public class GameActivity extends Activity implements Drawer {
 		GLSurfaceView glSurface = new GLSurfaceView(this);
 		setContentView(glSurface);
 		glSurface.setRenderer(new SpriteBatcher(this, resourceIds, this));
-		setupEntities();
+		display = getWindowManager().getDefaultDisplay();
+		displaySize = new Point();
+		display.getSize(displaySize);
+		setEntities();
 	}
 
-	private void setupEntities() {
-		this.background = new Background(this,0,0, resourceIds[0]);
+	private void setEntities() {
+		this.firstBackground = new Background(displaySize.y, this,0,0, resourceIds[0]);
+		this.secondBackground = new Background(displaySize.y, this, 0, 600, resourceIds[0]);
+	}
+
+	private void update() {
+		while(running) {
+			firstBackground.update();
+			secondBackground.update();
+			try {
+				Thread.sleep(1000);
+			} catch (InterruptedException e) {
+			}
+		}
 	}
 
 	@Override
 	public void onDrawFrame(GL10 gl, SpriteBatcher sb) {
-		sb.draw(background.getResourceId(), background.getSource(), background.getDestination());
+		sb.draw(firstBackground.getResourceId(), firstBackground.getSource(), firstBackground.getDestination());
+		sb.draw(secondBackground.getResourceId(), secondBackground.getSource(), secondBackground.getDestination());
 	}
 
 }
